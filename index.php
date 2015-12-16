9d3c6cedfba0d9ac9c96fb5e8c2a4a6d5aaa2a1f
Kevin Khew <k2leving@gmail.com>
2015-12-16 16:54:38 +0000

    update index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <?php echo '<meta http-equiv="refresh" content="0; url=countries.html" />' ?>
+        <title>Country List</title>
+    </head>
+    <body>
 
-echo '<meta http-equiv="refresh" content="0; url=countries.html" />';
+    </body>
+</html>
